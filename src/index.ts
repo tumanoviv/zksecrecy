569e9cb00b3c21df19a8193749f571f9cec1c65b
tumanoviv <82202528+tumanoviv@users.noreply.github.com>
2024-09-03 09:51:07 +0300

    add icon for form

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,40 @@
-import { greetUser } from '$utils/greet';
-// src/index.ts
-import './index.css';
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
-});
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+  faqAccordion();
+}
+
+function faqAccordion() {
+  const items = document.querySelectorAll('.faq__accordion-item');
+
+  if (items.length === 0) {
+    return; // Если нет элементов, функция завершается
+  }
+
+  function toggleAccordion(item) {
+    items.forEach((el) => {
+      const content = el.querySelector('.faq__content');
+      if (!content) {
+        return; // Пропустить, если контент не найден
+      }
+
+      if (el === item) {
+        el.classList.toggle('active');
+        content.style.height = el.classList.contains('active') ? content.scrollHeight + 'px' : '0';
+      } else {
+        el.classList.remove('active');
+        content.style.height = '0';
+      }
+    });
+  }
+
+  // Добавляем обработчики на каждый заголовок
+  items.forEach((item) => {
+    item.addEventListener('click', () => toggleAccordion(item));
+  });
+
+  // Открыть первый элемент сразу после загрузки страницы, если он существует
+  if (items.length > 0) {
+    toggleAccordion(items[0]);
+  }
+}
