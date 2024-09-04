4b3fa27a0a293b1c1b027fca314d0e04f5338b61
tumanoviv <82202528+tumanoviv@users.noreply.github.com>
2024-09-04 19:44:27 +0300

    add code

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,9 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
-roadmapLine();
-roadmapItem();
-heading();
+const showSite = () => {
+  gsap.to('.main', { autoAlpha: 1, duration: 1.5, ease: 'power1.inOut' });
+};
 
 function heading() {
   const text = new SplitType('.hero__title', { types: 'chars' });
@@ -17,6 +17,67 @@ function heading() {
   gsap.to(text.chars, { opacity: 1, duration: 3, ease: 'expo', stagger: 0.03, xPercent: 0 });
 }
 
+roadmap();
+
+function roadmap() {
+  const square = gsap.utils.toArray('.square');
+  const block = document.querySelectorAll('[roadmap="item"]');
+  const squareDur = 1;
+  const lineGreen = document.querySelector('.line__green');
+
+  const columnCenter = document.querySelector('.roadmap__column--center');
+  gsap.set(block, { opacity: 0 });
+
+  const lenght = lineGreen.getTotalLength();
+  gsap.set(lineGreen, { strokeDasharray: lenght });
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: columnCenter,
+      start: 'top bottom-=10%',
+      end: 'bottom+=80% bottom',
+      scrub: 1,
+    },
+  });
+  tl.fromTo(lineGreen, { strokeDashoffset: lenght }, { strokeDashoffset: 0, duration: 10 });
+  tl.addLabel('start', 0);
+  tl.to(
+    square[0],
+    { scale: 2, color: '#C4FE48', transformOrigin: 'center center', duration: squareDur },
+    0
+  );
+  tl.to(block[3], { opacity: 1, duration: 2 }, '0.3');
+  tl.to(block[2], { opacity: 1, duration: 2 }, '4.5');
+  tl.to(block[0], { opacity: 1, duration: 2 }, '0.5');
+  tl.to(block[1], { opacity: 1, duration: 2 }, '2.2');
+  tl.to(block[5], { opacity: 1, duration: 2 }, '3.5');
+  tl.to(block[4], { opacity: 1, duration: 2 }, '1.5');
+  tl.to(
+    square[1],
+    { scale: 2, color: '#C4FE48', transformOrigin: 'center center', duration: squareDur },
+    '0.55'
+  );
+  tl.to(
+    square[2],
+    { scale: 2, color: '#C4FE48', transformOrigin: 'center center', duration: squareDur },
+    '1.7'
+  );
+  tl.to(
+    square[3],
+    { scale: 2, color: '#C4FE48', transformOrigin: 'center center', duration: squareDur },
+    '2.7'
+  );
+  tl.to(
+    square[4],
+    { scale: 2, color: '#C4FE48', transformOrigin: 'center center', duration: squareDur },
+    '3.8'
+  );
+  tl.to(
+    square[5],
+    { scale: 2, color: '#C4FE48', transformOrigin: 'center center', duration: squareDur },
+    '4.6'
+  );
+}
 function roadmapLine() {
   const square = gsap.utils.toArray('.square');
   // square.forEach((item, index) => {
@@ -29,7 +90,13 @@ function roadmapLine() {
   gsap.set(lineGreen, { strokeDasharray: lenght });
 
   const tl = gsap.timeline({
-    scrollTrigger: { trigger: columnCenter, start: 'top center', scrub: true },
+    scrollTrigger: {
+      trigger: columnCenter,
+      start: 'top center',
+      end: 'bottom+=80% bottom',
+      scrub: true,
+      markers: true,
+    },
   });
   tl.fromTo(lineGreen, { strokeDashoffset: lenght }, { strokeDashoffset: 0 });
 }
@@ -76,11 +143,24 @@ function cardAnimation() {
     scrollTrigger: {
       trigger: '.features',
       scrub: 1,
+      start: 'top-=30% bottom',
+      end: 'bottom-=30% bottom',
     },
   });
-  tl.from(card[0], { yPercent: 40, opacity: 0 });
-  tl.from(card[1], { yPercent: 40, opacity: 0 }, '<');
+  tl.from(card[0], { yPercent: 30, opacity: 0, ease: 'power2.inOut' });
+  tl.from(card[1], { yPercent: 30, opacity: 0, ease: 'power2.inOut' }, '<');
 }
+
+const mobBurger = document.querySelector('.header__burger');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    // Условие прокрутки в пикселях
+    mobBurger.classList.add('scrolled');
+  } else {
+    mobBurger.classList.remove('scrolled');
+  }
+});
 
 // function numberSquare(indeEl) {
 //   const square = document.querySelectorAll('.square');
